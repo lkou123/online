648011d5e772005f2408731a4315a3b2ee5dd334
“lkou123” <lsl1065290576@163.com>
2020-01-03 18:05:27 +0800

    刘胜利，你是猪吗！！！！！！！！！！！！！！！！！！！！！！！！

--- bs/src/The.java
+++ bs/src/The.java
@@ -4,5 +4,7 @@ public class The {
     public static void main(String[] args) {
         System.out.println("刘胜利，你是猪吗！！！！！！！");
         System.out.println("刘胜利，你是猪吗！！！！！！！");
+        System.out.println("刘胜利，你是猪吗！！！！！！！");
+        System.out.println("刘胜利，你是猪吗！！！！！！！");
     }
 }
